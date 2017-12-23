e354964f6b7c5213d4868d292b1ab9ca3888ba00
pradithya aria <pradithya.aria@gmail.com>
2017-12-23 18:56:00 +0800

    Initial commit

---
+++ src/main/java/com/pradithya/chucknorrisjoke/ChucknorrisjokeApplication.java
@@ -0,0 +1,12 @@
+package com.pradithya.chucknorrisjoke;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChucknorrisjokeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChucknorrisjokeApplication.class, args);
+	}
+}
